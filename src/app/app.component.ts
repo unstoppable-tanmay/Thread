a655e7d2a2890669d7f56732321db0e13d5c8034
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2023-07-22 01:21:40 +0530

    adding redirect to home in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(
+      public router: Router
+  ) { }
+
   title = 'Threads';
+  ngOnInit() {  
+    this.router.navigate(['home']);
+  }
 }
